295dd40635d33701386858dc212825f657530ae0
Markus Brejla <m.brejla@gfx3d.net>
2025-06-08 23:17:01 +0200

    player class, circleshape class

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    game_dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        game_dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
