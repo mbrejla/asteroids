e3f98ca24d1526df954d95a42ebe3264cbb2974f
Markus Brejla <87433403+mbrejla@users.noreply.github.com>
2025-06-10 18:31:41 +0200

    remove pyright ignore

--- asteroidfield.py
+++ asteroidfield.py
@@ -30,7 +30,7 @@ class AsteroidField(pygame.sprite.Sprite):
     ]
 
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self, self.containers)  # pyright: ignore[reportAttributeAccessIssue]
+        pygame.sprite.Sprite.__init__(self, self.containers)
         self.spawn_timer = 0.0
 
     def spawn(self, radius, position, velocity):
